c4859036daf714c51d178d244fbaaae026bd1e5f
Mateusz Herych <heniek@applicake.com>
2013-08-14 18:17:18 +0200

    LazyCursorList first revision

---
+++ src/main/java/com/github/partition/android/database/loaders/LazyCursorList.java
@@ -0,0 +1,34 @@
+package com.github.partition.android.database.loaders;
+
+import com.google.common.base.Function;
+
+import android.database.Cursor;
+
+import java.util.AbstractList;
+
+public class LazyCursorList<T> extends AbstractList<T> {
+
+  private final Cursor cursor;
+  private final Function<Cursor, T> transformation;
+
+  public LazyCursorList(Cursor cursor, Function<Cursor, T> function) {
+    this.cursor = cursor;
+    this.transformation = function;
+  }
+
+  @Override
+  public T get(int i) {
+    // TODO - caching.
+    cursor.moveToPosition(i);
+    return transformation.apply(cursor);
+  }
+
+  @Override
+  public int size() {
+    return cursor.getCount();
+  }
+
+  Cursor getCursor() {
+    return cursor;
+  }
+}
