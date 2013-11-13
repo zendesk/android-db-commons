4b7c5acdd815fcefd1961cc9a4c94cacbd895b17
Jerzy Chalupski <jerzy@futuresimple.com>
2013-11-13 22:20:44 +0100

    Extract utility method for putting Object into ContentValues

--- library/src/main/java/com/getbase/android/db/provider/Update.java
+++ library/src/main/java/com/getbase/android/db/provider/Update.java
@@ -26,29 +26,9 @@ public class Update extends ProviderAction<Integer> {
     if (values == null) {
       values = new ContentValues();
     }
-    if (value == null) {
-      values.putNull(key);
-    } else if (value instanceof String) {
-      values.put(key, (String) value);
-    } else if (value instanceof Byte) {
-      values.put(key, (Byte) value);
-    } else if (value instanceof Short) {
-      values.put(key, (Short) value);
-    } else if (value instanceof Integer) {
-      values.put(key, (Integer) value);
-    } else if (value instanceof Long) {
-      values.put(key, (Long) value);
-    } else if (value instanceof Float) {
-      values.put(key, (Float) value);
-    } else if (value instanceof Double) {
-      values.put(key, (Double) value);
-    } else if (value instanceof Boolean) {
-      values.put(key, (Boolean) value);
-    } else if (value instanceof byte[]) {
-      values.put(key, (byte[]) value);
-    } else {
-      throw new IllegalArgumentException("bad value type: " + value.getClass().getName());
-    }
+
+    Utils.addToContentValues(key, value, values);
+
     return this;
   }
 
