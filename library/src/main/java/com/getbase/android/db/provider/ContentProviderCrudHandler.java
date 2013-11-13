5709872246c32abae492390301f3c8fb4983832c
Mateusz Herych <mateusz.herych@getbase.com>
2013-11-13 14:09:31 +0100

    CrudHandler for ContentProvider

---
+++ library/src/main/java/com/getbase/android/db/provider/ContentProviderCrudHandler.java
@@ -0,0 +1,36 @@
+package com.getbase.android.db.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.RemoteException;
+
+public class ContentProviderCrudHandler implements CrudHandler {
+
+  private final ContentProvider contentProvider;
+
+  public ContentProviderCrudHandler(ContentProvider contentProvider) {
+    this.contentProvider = contentProvider;
+  }
+
+  @Override
+  public Cursor query(Uri url, String[] projection, String selection, String[] selectionArgs, String sortOrder) throws RemoteException {
+    return contentProvider.query(url, projection, selection, selectionArgs, sortOrder);
+  }
+
+  @Override
+  public int delete(Uri url, String selection, String[] selectionArgs) throws RemoteException {
+    return contentProvider.delete(url, selection, selectionArgs);
+  }
+
+  @Override
+  public int update(Uri url, ContentValues values, String selection, String[] selectionArgs) throws RemoteException {
+    return contentProvider.update(url, values, selection, selectionArgs);
+  }
+
+  @Override
+  public Uri insert(Uri url, ContentValues initialValues) throws RemoteException {
+    return contentProvider.insert(url, initialValues);
+  }
+}
