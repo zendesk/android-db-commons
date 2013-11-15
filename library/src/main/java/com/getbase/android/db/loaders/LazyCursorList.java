12fd92b2165da1f835172158d06437f22f0667b6
Jerzy Chalupski <jerzy@futuresimple.com>
2013-11-15 12:02:14 +0100

    Add caching to LazyCursorList

--- library/src/main/java/com/getbase/android/db/loaders/LazyCursorList.java
+++ library/src/main/java/com/getbase/android/db/loaders/LazyCursorList.java
@@ -4,6 +4,7 @@ import com.getbase.android.db.cursors.Cursors;
 import com.google.common.base.Function;
 
 import android.database.Cursor;
+import android.support.v4.util.LruCache;
 
 import java.util.AbstractList;
 
@@ -11,17 +12,28 @@ class LazyCursorList<T> extends AbstractList<T> {
 
   private final Cursor cursor;
   private final Function<Cursor, T> transformation;
+  private final LruCache<Integer, T> cache;
 
   public LazyCursorList(Cursor cursor, Function<Cursor, T> function) {
+    this(cursor, function, 256);
+  }
+
+  public LazyCursorList(final Cursor cursor, Function<Cursor, T> function, int cacheSize) {
     this.cursor = Cursors.returnSameOrEmptyIfNull(cursor);
     this.transformation = function;
+
+    cache = new LruCache<Integer, T>(cacheSize) {
+      @Override
+      protected T create(Integer key) {
+        cursor.moveToPosition(key);
+        return transformation.apply(cursor);
+      }
+    };
   }
 
   @Override
   public T get(int i) {
-    // TODO - caching.
-    cursor.moveToPosition(i);
-    return transformation.apply(cursor);
+    return cache.get(i);
   }
 
   @Override
