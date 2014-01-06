76520193a4274427712eecc1ddb72e8c9343da00
Jerzy Chalupski <jerzy@futuresimple.com>
2014-01-06 15:54:59 +0100

    Fluent API for SQLite DELETE statement

---
+++ library/src/main/java/com/getbase/android/db/query/Delete.java
@@ -0,0 +1,74 @@
+package com.getbase.android.db.query;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.base.Functions;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Delete {
+  final String mTable;
+  final String mSelection;
+  final String[] mSelectionArgs;
+
+  private Delete(String table, String selection, String[] selectionArgs) {
+    mTable = table;
+    mSelection = selection;
+    mSelectionArgs = selectionArgs;
+  }
+
+  public static TableSelector delete() {
+    return new DeleteBuilder();
+  }
+
+  public int perform(SQLiteDatabase db) {
+    return db.delete(mTable, mSelection, mSelectionArgs);
+  }
+
+  public static class DeleteBuilder implements TableSelector, SelectionBuilder {
+    private String mTable;
+    private List<String> mSelections = Lists.newArrayList();
+    private List<String> mSelectionArgs = Lists.newArrayList();
+
+    private DeleteBuilder() {
+    }
+
+    @Override
+    public SelectionBuilder from(String table) {
+      mTable = checkNotNull(table);
+      return this;
+    }
+
+    @Override
+    public SelectionBuilder where(String selection, Object... selectionArgs) {
+      mSelections.add("(" + selection + ")");
+      mSelectionArgs.addAll(Collections2.transform(Arrays.asList(selectionArgs), Functions.toStringFunction()));
+
+      return this;
+    }
+
+    @Override
+    public Delete build() {
+      return new Delete(
+          mTable,
+          Joiner.on(" AND ").join(mSelections),
+          mSelectionArgs.toArray(new String[mSelectionArgs.size()])
+      );
+    }
+  }
+
+  public interface TableSelector {
+    SelectionBuilder from(String table);
+  }
+
+  public interface SelectionBuilder {
+    SelectionBuilder where(String selection, Object... selectionArgs);
+    Delete build();
+  }
+}
---
+++ library/src/test/java/com/getbase/android/db/query/DeleteTest.java
@@ -0,0 +1,42 @@
+package com.getbase.android.db.query;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class DeleteTest {
+
+  @Test
+  public void shouldBuildTheDeleteWithoutSelection() throws Exception {
+    Delete delete = Delete.delete().from("A").build();
+
+    assertThat(delete.mTable).isEqualTo("A");
+  }
+
+  @Test
+  public void shouldBuildTheDeleteWithSingleSelection() throws Exception {
+    Delete delete = Delete.delete().from("A").where("a IS NULL").build();
+
+    assertThat(delete.mSelection).isEqualTo("(a IS NULL)");
+  }
+
+  @Test
+  public void shouldBuildTheDeleteWithMultipleSelections() throws Exception {
+    Delete delete = Delete.delete().from("A").where("a IS NULL").where("b IS NULL").build();
+
+    assertThat(delete.mSelection).isEqualTo("(a IS NULL) AND (b IS NULL)");
+  }
+
+  @Test
+  public void shouldBuildTheDeleteWithBoundParams() throws Exception {
+    Delete delete = Delete.delete().from("A").where("a=?", 0).build();
+
+    assertThat(delete.mSelection).isEqualTo("(a=?)");
+    assertThat(delete.mSelectionArgs).containsOnly("0");
+  }
+}
