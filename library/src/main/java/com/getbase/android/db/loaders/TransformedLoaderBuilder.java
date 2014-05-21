fbcffe9d893a8f1cb84c0f6176b1df28044f9dc3
Jerzy Chalupski <jerzy.chalupski@gmail.com>
2014-05-22 00:05:15 +0200

    Allow registering multiple notification uris in Loader

--- library/src/main/java/com/getbase/android/db/loaders/TransformedLoaderBuilder.java
+++ library/src/main/java/com/getbase/android/db/loaders/TransformedLoaderBuilder.java
@@ -3,26 +3,34 @@ package com.getbase.android.db.loaders;
 import com.getbase.android.db.common.QueryData;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableList;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v4.content.Loader;
 
 public class TransformedLoaderBuilder<To> {
 
   private final QueryData queryData;
+  private final ImmutableList<Uri> notificationUris;
   private final Function<Cursor, To> wrapperFunction;
 
-  public TransformedLoaderBuilder(QueryData queryData, Function<Cursor, To> wrapperFunction) {
+  public TransformedLoaderBuilder(QueryData queryData, ImmutableList<Uri> notificationUris, Function<Cursor, To> wrapperFunction) {
     this.queryData = queryData;
+    this.notificationUris = notificationUris;
     this.wrapperFunction = wrapperFunction;
   }
 
+  public TransformedLoaderBuilder<To> addNotificationUri(Uri uri) {
+    return new TransformedLoaderBuilder<To>(queryData, ImmutableList.<Uri>builder().addAll(notificationUris).add(uri).build(), wrapperFunction);
+  }
+
   public <NewTo> TransformedLoaderBuilder<NewTo> transform(Function<To, NewTo> wrapper) {
-    return new TransformedLoaderBuilder<NewTo>(queryData, Functions.compose(wrapper, wrapperFunction));
+    return new TransformedLoaderBuilder<NewTo>(queryData, notificationUris, Functions.compose(wrapper, wrapperFunction));
   }
 
   public Loader<To> build(Context context) {
-    return new ComposedCursorLoader<To>(context, queryData, wrapperFunction);
+    return new ComposedCursorLoader<To>(context, queryData, notificationUris, wrapperFunction);
   }
 }
--- library/src/main/java/com/getbase/android/db/loaders/TransformedRowLoaderBuilder.java
+++ library/src/main/java/com/getbase/android/db/loaders/TransformedRowLoaderBuilder.java
@@ -4,10 +4,12 @@ import com.getbase.android.db.common.QueryData;
 import com.getbase.android.db.cursors.Cursors;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v4.content.Loader;
 
 import java.util.List;
@@ -15,28 +17,34 @@ import java.util.List;
 public class TransformedRowLoaderBuilder<T> {
 
   private final QueryData queryData;
+  private final ImmutableList<Uri> notificationUris;
   private final Function<Cursor, T> cursorTransformation;
 
-  public TransformedRowLoaderBuilder(QueryData queryData, Function<Cursor, T> transformation) {
+  public TransformedRowLoaderBuilder(QueryData queryData, ImmutableList<Uri> notificationUris, Function<Cursor, T> transformation) {
     this.queryData = queryData;
+    this.notificationUris = notificationUris;
     this.cursorTransformation = transformation;
   }
 
   public <Out> TransformedRowLoaderBuilder<Out> transformRow(final Function<T, Out> rowTransformer) {
-    return new TransformedRowLoaderBuilder<Out>(queryData, Functions.compose(rowTransformer, cursorTransformation));
+    return new TransformedRowLoaderBuilder<Out>(queryData, notificationUris, Functions.compose(rowTransformer, cursorTransformation));
   }
 
   public TransformedLoaderBuilder<List<T>> lazy() {
-    return new TransformedLoaderBuilder<List<T>>(queryData, getLazyTransformationFunction());
+    return new TransformedLoaderBuilder<List<T>>(queryData, notificationUris, getLazyTransformationFunction());
   }
 
   public <Out> TransformedLoaderBuilder<Out> transform(final Function<List<T>, Out> transformer) {
-    return new TransformedLoaderBuilder<Out>(queryData, Functions.compose(transformer, getEagerTransformationFunction())
+    return new TransformedLoaderBuilder<Out>(queryData, notificationUris, Functions.compose(transformer, getEagerTransformationFunction())
     );
   }
 
+  public TransformedLoaderBuilder<T> addNotificationUri(Uri uri) {
+    return new TransformedLoaderBuilder<T>(queryData, ImmutableList.<Uri>builder().addAll(notificationUris).add(uri).build(), cursorTransformation);
+  }
+
   public Loader<List<T>> build(Context context) {
-    return new ComposedCursorLoader<List<T>>(context, queryData, getEagerTransformationFunction());
+    return new ComposedCursorLoader<List<T>>(context, queryData, ImmutableList.copyOf(notificationUris), getEagerTransformationFunction());
   }
 
   private Function<Cursor, List<T>> getEagerTransformationFunction() {
